4b646d7fb3ca9521b7430e9eeb23ad554f00ce07
Tyler Hawkins <tylerhawkins@workfront.com>
2020-06-04 10:25:00 -0600

    sets the updated import map to be public

--- after_deploy.sh
+++ after_deploy.sh
@@ -3,5 +3,5 @@ aws s3 cp s3://single-spa-demo/@thawkin3/importmap.json importmap.json
 echo "Updating import map to point to new version of @thawkin3/root-config"
 node update-importmap.mjs
 echo "Uploading new import map to S3"
-aws s3 cp importmap.json s3://single-spa-demo/@thawkin3/importmap.json --cache-control 'public, must-revalidate, max-age=0'
+aws s3 cp importmap.json s3://single-spa-demo/@thawkin3/importmap.json --cache-control 'public, must-revalidate, max-age=0' --acl 'public-read'
 echo "Deployment successful"
